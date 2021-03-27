d34d2fc1af4deb6c50c42a5b89f6ec23a135715a
Ahmet Cihat Bozkurt <ahmetcihat2773@gmail.com>
2021-03-27 10:43:58 +0100

    Update filter.c

--- filter.c
+++ filter.c
@@ -1,47 +1,91 @@
-#include <stdio.h>
-#include <stdlib.h>
+#include "fdacoeffs.h"
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+#include <stdint.h>
 #define TRUE 1
 #define FALSE 0
-
+int16_t int16_sat(float x);
 struct HEADER {
 
-    unsigned char riff[4];                      // RIFF string
-	//unsigned char	1 byte
-    unsigned int overall_size   ;               // overall size of file in bytes
-	// unsigned int	2 or 4 bytes
-    unsigned char wave[4];                      // WAVE string
+    unsigned char riff[4];                      
 
-    unsigned char fmt_chunk_marker[4];          // fmt string with trailing null char
+    unsigned int overall_size   ;               
+    
+    unsigned char wave[4];                      
 
-    unsigned int length_of_fmt;                 // length of the format data
+    unsigned char fmt_chunk_marker[4];          
 
-    unsigned int format_type;                   // format type. 1-PCM, 3- IEEE float, 6 - 8bit A law, 7 - 8bit mu law
+    unsigned int length_of_fmt;                 
 
-    unsigned int channels;                      // no.of channels
+    unsigned int format_type;                   
 
-    unsigned int sample_rate;                   // sampling rate (blocks per second)
+    unsigned int channels;                     
 
-    unsigned int byterate;                      // SampleRate * NumChannels * BitsPerSample/8
+    unsigned int sample_rate;                   
+    unsigned int byterate;                     
 
-    unsigned int block_align;                   // NumChannels * BitsPerSample/8
+    unsigned int block_align;                 
 
-    unsigned int bits_per_sample;               // bits per sample, 8- 8bits, 16- 16 bits etc
-    unsigned char data_chunk_header [4];        // DATA string or FLLR string
+    unsigned int bits_per_sample;               
+    unsigned char data_chunk_header [4];        
 
-    unsigned int data_size;                     // NumSamples * NumChannels * BitsPerSample/8 - size of the next chunk that will be read
+    unsigned int data_size;                    
 
 };
-
 FILE *ptr;
 FILE *ptr_1;
 struct HEADER header;
 unsigned char buffer4[4];
 unsigned char buffer2[2];
-char* seconds_to_time(float seconds);
 int read = 0;
+int16_t int16_sat(float x)
+{
+	// 16 bits integer can hold maximum 32767 as decimal number. 
+	// In this function if the floating point is bigger than this number
+	// it is trancated to the maximum or minumum. Since the int16_t signed
+	// integer. And the float x which is coming from f32_fir function gives
+    if(x > 32767.0f)
+        return 0x7FFF;
+
+    if(x < -32768.0f)
+        return -0x8000;
+
+    return (int16_t)x;
+} // Converting float to 2byte integer.
+
+void fir_filter (int16_t *input_signal,int num_samples,int16_t *channel_filter)
+{	
+  int i, j, k;
+  float tmp;
+
+  for (k = 0; k < num_samples-1; k++) 
+  {
+    tmp = 0.0;
+
+	if (k<= b_len){
+		//printf("HESAP");
+		channel_filter[k] = 0;
+			
+	}else{
+			//printf("HESAP");	
+		    for (i = 0; i <b_len;i++) 
+		    {
+		      j = k - i; 
+			  if(j<=0)
+				break;
+
+		     tmp = tmp + (b_coeffs [i] * (float)input_signal [j]/32768.0f);
+			 //printf("%f",tmp);
+		    }
+	
+    channel_filter[k] = int16_sat(tmp*32768);
+    //printf("CHANNEL VALUE : %d",channel_filter[i]);
+    // convert float number into short int.
+  }
+}
+}
+
 int main(int argc, char **argv) {
 	
 	if(argc <2){
@@ -55,6 +99,8 @@ int main(int argc, char **argv) {
 		printf("Error opening file");
 		exit(1);
 	}
+
+	
 	// START READING
 	signed char temp[4];
 	read = fread(header.riff,sizeof(header.riff),1,ptr);
@@ -64,36 +110,32 @@ int main(int argc, char **argv) {
 	read = fread(buffer4, sizeof(buffer4), 1, ptr);
 	fwrite(buffer4, 1 , sizeof(buffer4) ,ptr_1);
 	// 5-8 byte
- 	printf("Chunk Size : (Liddle Endian):\t%u\t%u\t%u\t%u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
 	// convert little endian to big endian 4 byte int.
 	header.overall_size  = buffer4[0] |
                         (buffer4[1]<<8) |
                         (buffer4[2]<<16) |
                         (buffer4[3]<<24);
-    printf("(5-8) Overall size: bytes:%u, Kb:%u \n", header.overall_size, header.overall_size/1024);
     
  	read = fread(header.wave, sizeof(header.wave), 1, ptr);
  	fwrite(header.wave , 1 , sizeof(header.wave) ,ptr_1);
  	// 1 byte 9-12 since it is 4 bytes. 
- 	printf("(9-12) Wave marker: %s\n", header.wave);
 
 	read = fread(header.fmt_chunk_marker, sizeof(header.fmt_chunk_marker), 1, ptr);
 	fwrite(header.fmt_chunk_marker , 1 , sizeof(header.fmt_chunk_marker) ,ptr_1);
-	printf("(13-16) Fmt marker: %s\n", header.fmt_chunk_marker);
 	// here the fmt_chunk_marker is 4 bytes, that is why fread will move the pointer 4 bytes more.
 	
 	read = fread(buffer4, sizeof(buffer4), 1, ptr);
 	fwrite(buffer4 , 1 , sizeof(buffer4) ,ptr_1);
-	printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+
 	// again we are using the same buffer for parsing 4 bytes data.
 	
 	header.length_of_fmt = buffer4[0] |
                             (buffer4[1] << 8) |
                             (buffer4[2] << 16) |
                             (buffer4[3] << 24);
- 	printf("(17-20) Length of Fmt header: %u \n", header.length_of_fmt);
+
 	
-	read = fread(buffer2, sizeof(buffer2), 1, ptr); printf("%u %u \n", buffer2[0], buffer2[1]);
+	read = fread(buffer2, sizeof(buffer2), 1, ptr);
 	fwrite(buffer2 , 1 , sizeof(buffer2) ,ptr_1);
 	header.format_type = buffer2[0] | (buffer2[1] << 8);
 
@@ -104,137 +146,116 @@ char format_name[10] = "";
   strcpy(format_name, "A-law");
  else if (header.format_type == 7)
   strcpy(format_name, "Mu-law");
- printf("(21-22) Format type: %u %s \n", header.format_type, format_name);
+
  
 read = fread(buffer2, sizeof(buffer2), 1, ptr);
 fwrite(buffer2 , 1 , sizeof(buffer2) ,ptr_1);
-printf("%u %u \n", buffer2[0], buffer2[1]);
+
 
 header.channels = buffer2[0] | (buffer2[1] << 8);
-printf("(23-24) Channels: %u \n", header.channels);
+
 
 read = fread(buffer4, sizeof(buffer4), 1, ptr);
 fwrite(buffer4 , 1 , sizeof(buffer4) ,ptr_1);
- printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+
 
 
 header.sample_rate = buffer4[0] |
                         (buffer4[1] << 8) |
                         (buffer4[2] << 16) |
                         (buffer4[3] << 24);
- printf("(25-28) Sample rate: %u\n", header.sample_rate);
 
 read = fread(buffer4, sizeof(buffer4), 1, ptr);
 fwrite(buffer4 , 1 , sizeof(buffer4) ,ptr_1);
- printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+
 
 header.byterate  = buffer4[0] |
                         (buffer4[1] << 8) |
                         (buffer4[2] << 16) |
                         (buffer4[3] << 24);
 
-printf("(29-32) Byte Rate: %u , Bit Rate:%u\n", header.byterate, header.byterate*8);
 
 read = fread(buffer2, sizeof(buffer2), 1, ptr);
 fwrite(buffer2 , 1 , sizeof(buffer2) ,ptr_1);
-printf("LOOK AT HERE %u %u \n", buffer2[0], buffer2[1]);
+
  
 header.block_align = buffer2[0] |(buffer2[1] << 8);
-printf("(33-34) Block Alignment: %u \n", header.block_align);
+
 read = fread(buffer2, sizeof(buffer2), 1, ptr);
 fwrite(buffer2 , 1 , sizeof(buffer2) ,ptr_1);
-printf("%u %u \n", buffer2[0], buffer2[1]);
+
 header.bits_per_sample = buffer2[0] |
     (buffer2[1] << 8);
- printf("(35-36) Bits per sample: %u \n", header.bits_per_sample);
+
  read = fread(header.data_chunk_header, sizeof(header.data_chunk_header), 1, ptr);
  fwrite(header.data_chunk_header , 1 , sizeof(header.data_chunk_header) ,ptr_1);
- printf("(37-40) Data Marker: %s \n", header.data_chunk_header);
+
  read = fread(buffer4, sizeof(buffer4), 1, ptr);
  fwrite(buffer4 , 1 , sizeof(buffer4) ,ptr_1);
- printf("%u %u %u %u\n", buffer4[0], buffer4[1], buffer4[2], buffer4[3]);
+
  header.data_size = buffer4[0] |
                 (buffer4[1] << 8) |
                 (buffer4[2] << 16) |
                 (buffer4[3] << 24 );
- printf("(41-44) Size of data chunk: %u \n", header.data_size);
-  
+
+
  // calculate the number of sample
  long num_samples = (8 * header.data_size) / (header.channels * header.bits_per_sample);
- printf("Number of samples:%lu \n", num_samples);
+ num_samples = num_samples / 2 ; 
 
 long size_of_each_sample = (header.channels * header.bits_per_sample) / 8;
- printf("Size of each sample:%ld bytes \n ", size_of_each_sample);
- 
  // calculate duration of file
- float duration_in_seconds = (float) header.overall_size / header.byterate;
- printf("Approx.Duration in seconds=%f \n", duration_in_seconds);
- printf("Approx.Duration in h:m:s=%s \n", seconds_to_time(duration_in_seconds));
- long bytes_in_each_channel = (size_of_each_sample / header.channels);
+float duration_in_seconds = (float) header.overall_size / header.byterate;
+long bytes_in_each_channel = (size_of_each_sample / header.channels);
+
+long i =0;
+int16_t channel_0;
+int16_t channel_1;
+int16_t channel_zero[num_samples];
+int16_t channel_one[num_samples];
+printf("SIZE OF EACH SAMPLE : %d\n",size_of_each_sample);
+printf("BYTES IN EACH CHANNEL : %d\n",bytes_in_each_channel);
+printf("NUMBER OF SAMPLES : %d\n",num_samples);
 int byt_read;
- if (header.format_type == 1) {
-        long i =0;
-        long bytes_in_each_channel = (size_of_each_sample / header.channels);
-        printf("BYTES IN EACH CHANNEL% ld",bytes_in_each_channel);               
-        char channel_0[bytes_in_each_channel];
-        char channel_1[bytes_in_each_channel];
-		for (i =1; i <= num_samples; i++) {  
-			read = fread(channel_0, sizeof(channel_0), 1, ptr);
-			read = fread(channel_1, sizeof(channel_1), 1, ptr);                                               
-            fwrite(channel_1 , 1 , sizeof(channel_1) ,ptr_1);
-			fwrite(channel_0 , 1 , sizeof(channel_0) ,ptr_1);   							      
-        }     
-     }
-     do{
-     	byt_read = fread(buffer2,sizeof(buffer2),1,ptr);
-     	fwrite(buffer2, 1 , sizeof(buffer2) ,ptr_1);     	
-	 }while(byt_read>0);
- 	fclose(ptr_1);
- 	free(argv[2]);
- 	fclose(ptr);
-	free(argv[1]);
-	return 0;
+ if (header.format_type == 1) {		
+	for (i =1; i <= num_samples; i++) {  
+		// Read data as  int16_t for channel_0
+		read = fread(&channel_0, sizeof(channel_0), 1, ptr);
+		channel_zero[i] = channel_0;
+		// Read data as int16_t for channel_1	
+		read = fread(&channel_1, sizeof(channel_1), 1, ptr);
+		channel_one[i] = channel_1;
+		// Each sample is 1 byte and misunderstanding is removed. 
+		}
+}
+int16_t *channel_zero_filter;
+int16_t *channel_one_filter;
+channel_zero_filter = (int16_t*)malloc(num_samples*sizeof(int16_t));
+channel_one_filter= (int16_t*)malloc(num_samples*sizeof(int16_t));	
+
+fir_filter(channel_zero,num_samples,channel_zero_filter);
+printf("BEFORE FILTER");
+fir_filter(channel_one,num_samples,channel_one_filter);
+int k;
+for (k = 0; k < (num_samples-1); k++)  //  position in output
+{
+	//printf("AFTER FILTER : %d",channel_zero_filter[k]);
+	fwrite(&channel_zero_filter[k],size_of_each_sample,1, ptr_1);
+	fwrite(&channel_one_filter[k],size_of_each_sample,1, ptr_1);
 }
 
-char* seconds_to_time(float raw_seconds) {
+// After writing the data, write the rest of the file.
+ do{
+ 	byt_read = fread(buffer2,sizeof(buffer2),1,ptr);
+ 	fwrite(buffer2, 1 , sizeof(buffer2) ,ptr_1);     	
+ }while(byt_read>0);
 
-  char *hms;
 
-  int hours, hours_residue, minutes, seconds, milliseconds;
 
-  hms = (char*) malloc(100);
-
-  sprintf(hms, "%f", raw_seconds);
-
-  hours = (int) raw_seconds/3600;
-
-  hours_residue = (int) raw_seconds % 3600;
-
-  minutes = hours_residue/60;
-
-  seconds = hours_residue % 60;
-
-  milliseconds = 0;
-
-  // get the decimal part of raw_seconds to get milliseconds
-
-  char *pos;
-
-  pos = strchr(hms, '.');
-
-  int ipos = (int) (pos - hms);
-
-  char decimalpart[15];
-
-  memset(decimalpart, ' ', sizeof(decimalpart));
-
-  strncpy(decimalpart, &hms[ipos+1], 3);
-
-  milliseconds = atoi(decimalpart);
-
-  sprintf(hms, "%d:%d:%d.%d", hours, minutes, seconds, milliseconds);
-  return hms;
+fclose(ptr_1);
+free(argv[2]);
+fclose(ptr);
+free(argv[1]);	
+return 0;
 
 }
-
-
